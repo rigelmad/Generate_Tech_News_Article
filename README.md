72fa98aaf7c7915374838574edefae53f4764e93
Rigel <rlyankees@gmail.com>
2019-08-07 17:01:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Generate Tech News Article
+
+## Purpose
+This is a utility used to auto-generate a tech news article based on
+specified parameters. The user should specify parameters in the `config.json` file, and then use the `generate_article.py` script to generate a coherent news article from the specified parameters.
+
+## Documentation
+### config.json
+#### Configuration Paramters
+**file_name**: The desired file name for this article
+
+**thumbnail_picture**: The URL of the picture to appear on a website thumbnail.
+
+**headline**: The headline of the article, keep between 5-12 words. Mention the name of a tech company or service for added attention.
+
+**subtitle**: The subtitle, keep to one sentence. Reader should be able to understand the crux of the article by now.
+
+**source**: The source of the information being addressed.
+- In this release, the supported `type`s are:
+```
+"video",
+"press_release",
+"quote_from_source",
+"tweet",
+"other"
+```
+  - Sources with defined types (any type that is not `other`) will automatically be parsed and given a description using through a Machine Learning algorithm.
+  - Sources of `"type" : "other"` must then be given a `manual_description` field to describe what the information is relaying.
+- The `url` field indicates the source of the information, and will be [hyperlinked](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet#links).
+
+**previous_knowledgebase**: Articles that detail any previous information that is known about this product.
+- The `url` tag is the location of the information, a Machine Learning algorithm will go through and draw analyses from each of the URLs specified, centering upon the topic of _this_ article.
+- Any additional information can be specified in the `manual_analysis` portion, and will be fed into the creation of the description.
+- A max of 3 paragraphs will be created to keep article length appropriate.
+
+**future_expected_information**: When can we expect more information about this product, and through what medium?
+- In this version, supported `type`s are:
+```
+"press_conference",
+"product_release",
+"convention",
+"other"
+```
+  - Items with a `"type" : "other"` will require a `manual_description`.
+
+#### Other Notes
+- This version supports pseudo-markdown, meaning that in any manual description, a user may add two asterisks (\*\*) on either side of a statement to make that statement appear as a **BOLD SIDEBAR**, in proximity of that statement in the article. Sidebars will be rejected if they are greater than **10 words**.
+
+### generate_article.py
+#### Methodology
+Run this script after specifying the parameters in `config.json` in order to create a news article. The script uses [INSERT COMPLICATED MACHINE LEARNING ALGORITHM NAME HERE] to parse information from sources, and uses natural language processing provided by the Google Home API to generate a coherent article.
---
+++ generate_article.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+
+#####################################################################
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2019, Rigel Madraswalla.
+# All rights reserved.
+#####################################################################
+
+import json
+import some_complicated_machine_learning_algorithm as machine_learning
+
+# Creates a news aricle and saves it to the specified file name.
+def make_article(config):
+
+    # Create a blank article, an item that we can put our text onto,
+    #  eg. the final news article
+    article_file = open_file(config["file_name"])
+
+    # Attach the thumbnail image at the specified url
+    attach_thumbnail_image(config["thumbnail_picture"], article_file)
+
+    # Handle the headline, evaulate and write the headline to the article
+    #  document
+    handle_headline(config["title"], article_file)
+
+    # Handle the subtitle
+    handle_subtitle(config["subtitle"], article_file)
+
+    # Create 1 paragraph outlining what the new information is,
+    #  and what source it came from.
+    handle_source(config["source"], article_file)
+
+    # Create paragraphs (up to 3) detailing previous knowledge about the product
+    #  based on other articles
+    handle_previous_knowledgebase(config["previous_knowledgebase"], \
+        article_file)
+
+    # Create 1 paragraph outlining when we can expect future information to be
+    #  released/uncovered regarding this product
+    handle_future_expected_information(config["future_expected_information"], \
+        article_file)
+
+################## HELPER FUNCTIONS ###################
+
+# Evaluates the headline size and writes the headline to the article
+def handle_headline(headline, article_file):
+    # Check the size of the headline
+    if (headline.number_of_words() <= 14):
+        # Write the headline of the article
+        article_file.write_in_english(
+            content = headline,
+            type = "headline"
+        )
+    else:
+        # Raise an error if the headline is too long
+        raise ("Article headline is above average length, \
+                reduce words or headline will not fit \
+                on a website thumbnail!")
+
+# Evaluates the subtitle size and writes the subtitle to the article
+def handle_subtitle(subtitle, article_file):
+    # Check the size of the subtitle
+    if (subtitle.number_of_sentences() == 1):
+        # Write the subtitle of the article
+        article_file.write_in_english(
+            content = subtitle,
+            type = "subtitle"
+        )
+    else:
+        # Raise an error if the title is too long
+        raise ("Article must have a subtitle of EXACTLY 1 sentence.")
+
+# Accesses the URL of the source and uses machine learning to develop a
+#  source paragraph
+def handle_source(source, article_file):
+
+    # If the source is of type other, use the manual description
+    if (source["type"] == "other"):
+        description = source["manual_description"]
+
+    # Else, use machine learning to generate a description from url
+    else:
+        description = machine_learning.generate_description_from_url(
+            type = source["type"],
+            url = source["url"]
+        )
+
+    # Write the description, manual or machine learning, to the article
+    article_file.write_in_english(
+        content = description,
+        type = "paragraph"
+    )
+
+# Create up to 3 paragraphs amalgamating all previous sources included
+def handle_previous_knowledgebase(previous_knowledgebase, article_file):
+
+    # Go through each one of the specified sources and generate a description
+    #  of what that source tells us about our current topic
+    for each_entry in previous_knowledgebase:
+        description.append(
+            machine_learning.generate_description_from_url(
+                url = each_entry["url"],
+                addtional_info = each_entry["manual_analysis"]))
+
+    # Trim the description to max 3 paragraphs to ensure short article length
+    description.trim_the_fat(max_paragraphs = 3)
+
+    # Write the description to the article
+    article_file.write_in_english(
+        content = description,
+        type = "paragraph"
+    )
+
+# Create 1 paragraph detailing when we can expect more information on this topic
+def handle_future_expected_information(future_expected_information, \
+                                        article_file):
+
+    # If the type of info is unsupported, use the provided manual_description
+    if (future_expected_information["type"] == "other"):
+        description = future_expected_information["manual_description"]
+
+    # Else, use machine_learning to generate a blurb based on key info
+    else:
+        description = machine_learning.generate_future_info_blurb(
+            type = future_expected_information["type"],
+            name = future_expected_information["name"],
+            date = future_expected_information["date"],
+        )
+
+    # Write the paragraph to the article
+    article_file.write_in_english(
+        content = description,
+        type = paragraph
+    )
+
+# Retrieves the parameters specified in config.json
+# @return The JSON of parameters
+def get_config():
+    with open("config.json", "r+") as config_file:
+        return json.loads(config_file.read())
+
+if __name__ == "__main__":
+    config = get_config()
+    make_article(config)
